a8f327faa96734cec995a07d5bf1d7b60d93924c
Chuma Nnaji <cnnaji2003@gmail.com>
2012-01-17 16:10:11 -0800

    merging

---
+++ webservice/index.php
@@ -0,0 +1,14 @@
+<?php
+mysql_connect('localhost', 'root', '');
+mysql_select_db('limitless');
+$stock = $_GET['stock'];
+$sql = 'select UNIX_TIMESTAMP(date), close_price from company_' . strtoupper($stock) . ' ORDER BY date ASC';
+$res = mysql_query($sql) or die(mysql_error());
+$all = array();
+while ($arr = mysql_fetch_array($res, MYSQL_NUM)) {
+		$arr[0] = intval($arr[0])*1000;
+		$arr[1] = floatval($arr[1]);
+		$all[] = $arr;
+}
+echo json_encode($all);
+?>
