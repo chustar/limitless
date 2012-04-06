8a8cc1506e7e13c612aaff7eb0a69859f99faf65
Minh <aminh101sj@gmail.com>
2012-04-06 13:19:39 -0700

    added prediction to stock on webpage

---
+++ ai/bayes/create_macd_5_table.rb
@@ -0,0 +1,45 @@
+# This file inserts the historgram data into table volume 
+require 'mysql'
+require 'date'
+
+#my = Mysql.new(hostname, username, password, databasename)
+con = Mysql.new('localhost', 'root', '', 'limitless')
+rs = con.query('show tables')
+rs.each_hash { |h| 
+                        
+										if h['Tables_in_limitless'] =~ /company_.*/
+                    q = con.query("select * from #{h['Tables_in_limitless']}")
+                    table = h['Tables_in_limitless']
+										name = h['Tables_in_limitless'].split('_',2)
+										volume = 0
+										count = 0
+										avg = 0
+										puts "#{h['Tables_in_limitless']}"
+										q.each_hash { |a| 
+																			date = a['date']
+                                      s = a['date'].split('-')
+                                      date2 = Date.new(s[0].to_i, s[1].to_i, s[2].to_i) + 5 
+                                        
+																			cur_price = a['close_price']
+																			if a['histogram'].to_f < 0.50 && a['histogram'].to_f > 0 && Date.parse("#{date}") < Date.parse("2011-9-28")
+																				# if volumen is higher than 50% and closing price is higher than 25%
+																					theFUTURE = con.query("select * from #{h['Tables_in_limitless']} where date='#{date2}' LIMIT 1")
+                                    		  theFUTURE.each_hash { |f1|
+                                    		      price = f1['close_price'].to_f - cur_price.to_f 
+                                    		      price = price.to_f/cur_price.to_f 
+                                    		      price *= 100 
+                                    		      if price > 3 
+																								#puts "#{price}"
+                                    		        #puts "YES #{date2}: #{f1['close_price']} #{date}: #{cur_price}"
+                                    		        con.query("INSERT INTO macd5 (date, company, histogram, up) VALUES ('#{date}', '#{h['Tables_in_limitless']}', '#{a['histogram']}', '1')")
+                                    		      else
+                                    		        #puts "NO #{date2}: #{f1['close_price']} #{date}: #{cur_price}"
+                                    		        con.query("INSERT INTO macd5 (date, company, histogram, up) VALUES ('#{date}', '#{h['Tables_in_limitless']}', '#{a['histogram']}', '0')")
+                                    		      end 
+																					}
+																			end
+                                }
+                    end 
+              }   
+
+con.close
---
+++ webservice/bayes.php
@@ -0,0 +1,92 @@
+<?php
+// JSON data structure 
+// 0: ema indicator (1 means it has the attribute, 0 otherwise)
+// 1: macd indicator (1 means it has the attribute, 0 otherwise)
+// 2: volume indicator (1 means it has the attribute, 0 otherwise, -1 means table doesn't have average)
+// 3: ema up percentage
+// 4: ema down percentage
+// 5: macd up percentage
+// 6: mad down percentage
+// 7: volume up percentage
+// 8: volume down percentage
+
+// set the default timezone to use. Available since PHP 5.1
+date_default_timezone_set('UTC');
+
+mysql_connect('localhost', 'root', '') or die("mysql connect: " . mysql_error());
+mysql_select_db('limitless');
+$all = array();
+$stock = $_GET['stock'];
+
+
+//query statements for bayes analysis
+$sql_get_date = 'select date from company_' . strtoupper($stock) . ' ORDER BY date desc LIMIT 1';
+$sql_avg_volume = 'select avg_volume from companies where symbol=' . "'" . strtoupper($stock) . "'";
+$sql_stats = 'select * from bayes_prediction_22days';
+
+//get latest date for analysis
+$res = mysql_query($sql_get_date) or die(mysql_error());
+while ($arr = mysql_fetch_array($res, MYSQL_ASSOC)) {
+	$cur_date = $arr['date'];
+}
+$sql_stock = 'select macd, histogram, volume from company_' . strtoupper($stock) . ' where date='. "'" . $cur_date . "'" .' ORDER BY date ASC';
+
+//get average volume to compare with latest company's statistics
+$res = mysql_query($sql_avg_volume) or die(mysql_error());
+while( $arr = mysql_fetch_array($res, MYSQL_ASSOC)) {
+	$avg_volume = $arr['avg_volume'];
+}
+
+//get current stock data and assign prediction
+// 1 = follows past prediction
+// 0 = doesn't follow past prediction
+// -1 = prediction cannot be made
+$avg_set = isset($avg_volume);
+$res = mysql_query($sql_stock) or die (mysql_error());
+while ( $arr = mysql_fetch_array($res, MYSQL_ASSOC)) {
+	//ema analysis
+	if($arr['macd'] < 0.50 && $arr['macd'] > 0) {
+		$all[] = "1";
+	} else {
+		$all[] = "0";
+	}
+
+	//macd analysis
+	if ($arr['histogram'] < 0.50 && $arr['histogram'] > 0) {
+		$all[] = "1";
+	} else {
+		$all[] = "0";
+	}
+
+	//volume analysis
+	if($avg_set) {
+		$high_vol = (($arr['volume'] - $avg_volume)/$arr['volume'])*100;
+		if( $high_vol > 25) {
+			$all[] = "1";	
+		} else {
+			$all[] = "0";
+		}
+	} else {
+		$all[] = "-1";
+	}
+}
+
+//get bayes prediction values based on past history
+$res = mysql_query($sql_stats) or die(mysql_error());
+while ($arr = mysql_fetch_array($res, MYSQL_NUM)) {
+		$ema_up =   (string)$arr[1];
+		$ema_down =  (string)$arr[2];
+		$macd_up =  (string)$arr[4];
+		$macd_down =  (string)$arr[5];
+		$v_up =  (string)$arr[7];
+		$v_down =  (string)$arr[8];
+		$all[] = $ema_up; 
+		$all[] = $ema_down;
+		$all[] = $macd_up;
+		$all[] = $macd_down;
+		$all[] = $v_up;
+		$all[] = $v_down;
+}	
+
+echo json_encode($all, JSON_FORCE_OBJECT);
+?>
