1b236aacebf1fa9406fb928055e20ea871b5fc92
Minh <aminh101sj@gmail.com>
2012-04-13 16:46:28 -0700

    merged

--- website/index.php
+++ website/index.php
@@ -9,7 +9,8 @@
 		<script type="text/javascript">
 			var count = 0;
 			var series = new Array();
-			var stock = '<?= $_GET['stock'] ?>';
+			//var stock = '<?= $_GET['stock'] ?>';
+			var stock = 'msft';
 			var createChart = function() {
 				window.chart = new Highcharts.StockChart({
 					chart : {
@@ -31,7 +32,7 @@
 				});
 			};
 			$(function() {
-				$.getJSON('/limitless/webservice/index.php?stock=' + stock, function(data) {
+				$.getJSON('../webservice/index.php?stock=' + stock, function(data) {
 					series.push({'name': stock, 'data': data });
 					count++;
 					if(count == 2) {
@@ -39,7 +40,7 @@
 					}
 				});
 
-				$.getJSON('/limitless/webservice/predictions.php?stock=' + stock, function(data) {
+				$.getJSON('../webservice/predictions.php?stock=' + stock, function(data) {
 					series.push({'name': stock + ' Prediction', 'data': data });
 					count++;
 					if(count == 2) {
@@ -47,12 +48,74 @@
 					}
 				});
 
-				$.getJSON('/limitless/webservice/company.php?stock=' + stock, function(data) {
+				$.getJSON('../webservice/company.php?stock=' + stock, function(data) {
 					document.title = data.name;
 					console.log(data);
 					$('#company_name').text(data.name);
 				});
+
 			});
+		</script>
+		  <script>
+  
+  $(document).ready(function(){
+    //var stock = '<?= $_GET['stock'] ?>'; 
+    var stock = 'xata';
+		$.ajax({
+        url: '../webservice/bayes.php',
+        data: 'stock=' + stock,
+        dataType: 'json' 
+      }).done(function(data) {
+        var total_up = 1;
+        var total_down = 1;
+        var e = parseInt(data[0]);
+        var m = parseInt(data[1]);
+        var v = parseInt(data[2]);
+        if(e) {
+        
+          total_up *= data['3']/100;
+          total_down *= data['4']/100;
+
+          $("#ema_msg").html("");
+          $("#ema_up").html(parseFloat(data['3']).toFixed(2) + "% UP"); 
+          $("#ema_down").html(parseFloat(data['4']).toFixed(2) + "% DOWN"); 
+          
+        } 
+        if(m) {
+          
+          total_up *= data['5']/100;
+          total_down *= data['6']/100;
+
+          $("#macd_msg").html("");
+          $("#macd_up").html(parseFloat(data['5']).toFixed(2) + "% UP");  
+          $("#macd_down").html(parseFloat(data['6']).toFixed(2) + "% DOWN");  
+        }
+        if(v) {
+
+          total_up *= data['7']/100;
+          total_down *= data['8']/100;
+
+          $("#volume_msg").html("");
+          $("#volume_up").html(parseFloat(data['7']).toFixed(2) + "% UP");  
+          $("#volume_down").html(parseFloat(data['8']).toFixed(2) + "% DOWN");
+        }
+        var tmp_up, tmp_down; 
+        tmp_up = total_up/(total_up + total_down);
+        tmp_down = total_down/(total_up + total_down);
+        total_up = tmp_up;
+        total_down = tmp_down;
+        if(e || m || v) {
+          $('#total_up').html("Total Percent Rise: " + parseFloat(total_up*100).toFixed(2) + "%");
+          $('#total_down').html("Total Percente Drop: " + parseFloat(total_down*100).toFixed(2) + "%");
+      
+          if(parseFloat(total_up*100).toFixed(2) > parseFloat(total_down*100).toFixed(2) ) {
+            $('#indicator').html('UP');
+          } else {
+            $('#indicator').html('DOWN');
+          } 
+        } 
+      });
+  }); 
 		</script>
 	</head>
 	<body>
@@ -83,36 +146,41 @@
 					MACD GRAPH
 				</div>
 			</div>
-			<div class="grid_4" id="ai_data">
-				<h1> Groovy Analysis </h1>
-				<div class="grid_4">
-					<h2>MACD</h2>
-					<div class="grid_4">mean =  </div>	
-					<div class="grid_4">std. div =  </div>
-					<div class="grid_4">Probabilty of MACD =  </div>
-				</div>
-				<div class="grid_4">
-					<h2>Volume</h2>
-					<div class="grid_4">mean =  </div>  
-					<div class="grid_4">std. div =  </div>
-					<div class="grid_4">Probabilty of Volume =  </div>
-				</div>
-				<div class="grid_4">
-					<h2>EMA</h2>
-					<div class="grid_4">mean =  </div>  
-					<div class="grid_4">std. div =  </div>
-					<div class="grid_4">Probabilty of EMA =  </div>
-				</div>
-				<div class="grid_4">
-					<h2>Total Probability</h2>
-					<div class="grid_4" >Total probability =  </div>
-				</div>
-				<div class="clear spacer"></div>	
-			</div>	
-
-			<div class="clear spacer"></div> 
-			<div class="grid_12" id="footer">  
-			</div>  
-		</div>
+		  <div class="grid_4" id="ai_data">
+ 			 <h1> Groovy Analysis </h1>
+ 			 <div class="grid_4" id="indicator"> </div>
+ 			   <div class="grid_4">
+ 			     <h2>EMA</h2>
+ 			       <div class="grid_4" id="ema_msg"> NO INDICATOR </div>
+ 			       <div class="grid_4" id="ema_up"></div>  
+ 			       <div class="grid_4" id="ema_down"></div>
+ 			       <div class="grid_4" id="ema"></div>
+ 			   </div>
+ 			   <div class="grid_4">
+ 			     <h2>MACD</h2>
+ 			       <div class="grid_4" id="macd_msg"> NO INDICATOR</div>
+ 			       <div class="grid_4" id="macd_up"></div> 
+ 			       <div class="grid_4" id="macd_down"></div>
+ 			       <div class="grid_4" id="macd"></div>
+ 			   </div>
+ 			   <div class="grid_4">
+ 			     <h2>Volume</h2>
+ 			       <div class="grid_4" id="volume_msg"> NO INDICATOR</div>
+ 			       <div class="grid_4" id="volume_up"></div>  
+ 			       <div class="grid_4" id="volume_down"></div>
+ 			       <div class="grid_4" id="volume"></div>
+ 			   </div>
+ 			   <div class="grid_4">
+ 			     <h2>Total Probability</h2>
+ 			       <div class="grid_4" id="total_up"> No Analysis</div>
+ 			       <div class="grid_4" id="total_down"></div>
+ 			   </div>
+ 			   <div class="clear spacer"></div>  
+ 			 </div>  
+ 			   
+ 			 <div class="clear spacer"></div> 
+ 			 <div class="grid_12" id="footer">  
+ 			 </div>
+	</div>
 	</body>
 </html>
